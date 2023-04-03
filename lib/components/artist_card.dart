f11ff148b1b65c6b92536433d8d0b38eeb0c0c74
Yash-Bavishi <bavishiys63@gmail.com>
2023-04-03 18:24:58 +0530

    TEST

--- lib/components/artist_card.dart
+++ lib/components/artist_card.dart
@@ -12,63 +12,31 @@ class Artists extends StatefulWidget {
 class _ArtistsState extends State<Artists> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+		return Scaffold(
 			body: Center(
-			child: Container(
-				height: 178.0,
-				width: 327.0,
-				child: Column(
-					children: [
-						
-					/*
-						Button
-					*/
-
-					ElevatedButton(
-						onPressed: () {
-								print('hi');
-							},
-						child: Text('ARTIST 1', style: TextStyle(fontFamily: 'Archivo', fontWeight: FontWeight.w600, letterSpacing: 2.4)),
-						style: ButtonStyle(
-							backgroundColor: MaterialStatePropertyAll<Color>(Color.fromRGBO(180, 86, 186, 1)),
-							padding: MaterialStatePropertyAll<EdgeInsetsGeometry>(EdgeInsets.fromLTRB(15.0,11.0,15.0,11.0)),
-							shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
-									RoundedRectangleBorder(
-										borderRadius: BorderRadius.circular(32.0),
-										side: BorderSide(color: Colors.red)
-									)	
+					child: Container(
+						width: 327.0,
+						height: 178.0,
+						decoration: BoxDecoration(
+							borderRadius: BorderRadius.circular(24),
+							color: Color.fromRGBO(101, 42, 112, 1),
+						),
+						child: Column(
+							children: [
+								Container(
+									width: 85.0,
+									height: 35.0,
+									decoration: BoxDecoration(
+										borderRadius: BorderRadius.circular(32),
+										color: Color.fromRGBO(180,86,186,1),
 								),
-							
-							),
-
+									child: Text('KING IS HERE')
+								)
+							],	
+						)
 					),
-
-					Row(
-						children: [
-							Text('Billie Eilish', style: TextStyle(fontFamily: 'Archivo', fontSize: 24.0, fontWeight: FontWeight.w700, color: Colors.white)),
-							Row(
-										children: [
-											ElevatedButton(
-												onPressed: () { print('hello'); },
-												child: Icon(Icons.arrow_left),
-												style: ElevatedButton.styleFrom(
-													shape: RoundedRectangleBorder(
-														borderRadius: BorderRadius.circular(50)
-													)
-												)
-											)	
-									],
-										crossAxisAlignment: CrossAxisAlignment.end,
-										mainAxisAlignment: MainAxisAlignment.spaceBetween,
-									)
-						],
-							),
-					],
-					mainAxisAlignment: MainAxisAlignment.spaceBetween,
-				),	
-),
 				),
-			backgroundColor: Colors.black
-			);
+			backgroundColor: Colors.black,
+		);
   }
 }
